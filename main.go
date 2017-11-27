5b09533e4dc2194022a03e365c89cd94bb7fb661
Patrick Wynne <pswoutlaw@gmail.com>
2017-11-27 09:05:58 -0600

    Update main.go

--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
     "fmt"
     "net/http"
-    "strings"
-    "log"
+    "io/ioutil"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()  // parse arguments, you have to call this by yourself
-    fmt.Println(r.Form)  // print form information in server side
-    fmt.Println("path", r.URL.Path)
-    fmt.Println("scheme", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key:", k)
-        fmt.Println("val:", strings.Join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello astaxie!") // send data to client side
+type Page struct {
+    Title string
+    Body  []byte
 }
-
 func main() {
-    http.HandleFunc("/", sayhelloName) // set router
-    err := http.ListenAndServe(":9090", nil) // set listen port
+    http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/html/", htmlHandler)
+    http.ListenAndServe(":9090", nil)
+}
+//a very simple handler returns a string
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+//handler returns an html page composed of the url path parameter and html content from file
+func htmlHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/view/"):]
+    p, _ := loadPage(title)
+    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+//compiles the actual page to be returned
+func loadPage(title string) (*Page, error) {
+    filename := title + ".html"
+    body, err := ioutil.ReadFile(filename)
     if err != nil {
-        log.Fatal("ListenAndServe: ", err)
+        return nil, err
     }
+    return &Page{Title: title, Body: body}, nil
 }
