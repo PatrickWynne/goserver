6a26cbb4e2c9d9455d97feea8c0f799db126075c
pwynne <pwynne@flex125.com>
2017-11-28 08:11:16 -0600

    added text handler

--- main.go
+++ main.go
@@ -25,26 +25,30 @@ type User struct{
 }
   
 func main() {
+    http.HandleFunc("/text/", textHandler)
     http.HandleFunc("/view/", viewHandler)
     http.HandleFunc("/html/", htmlHandler)
     http.HandleFunc("/jsonResult/", jsonResultHandler)
     http.HandleFunc("/apiConsumer/", apiConsumerHandler)
     http.ListenAndServe(":9090", nil)
 }
-//a very simple handler returns a string
+func textHandler(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "Hello World")
+}
+//return render html + string
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "text/html; charset=utf-8")
     urlString := strings.Replace(r.URL.Path[1:], "/", "", 2)
     fmt.Fprintf(w, "Hi there, I love %s! \n", urlString)
     fmt.Fprint(w, "<a href='/html/gopage'>Click here to see a new page</a>")
 }
-//handler returns an html page composed of the url path parameter and html content from file
+//return render html
 func htmlHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
     fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
-//returns an html page if available using the url path parameter provided
+//return render [page].html
 func loadPage(title string) (*Page, error) {
     filename := title + ".html"
     body, err := ioutil.ReadFile(filename)
@@ -53,7 +57,7 @@ func loadPage(title string) (*Page, error) {
     }
     return &Page{Title: title, Body: body}, nil
 }
-//return json
+//return json result
 func jsonResultHandler(w http.ResponseWriter, r *http.Request) {
     jsonObject := JsonObject{1, "Programmer"}  
     js, err := json.Marshal(jsonObject)
@@ -64,7 +68,7 @@ func jsonResultHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Set("Content-Type", "application/json")
     w.Write(js)
   }
-  //consume a rest api and return the json result
+  //return external RESTful API json result [GET]
   func apiConsumerHandler(w http.ResponseWriter, r *http.Request){
     url := "https://jsonplaceholder.typicode.com/posts/3"
     req, err := http.NewRequest("GET", url, nil)
